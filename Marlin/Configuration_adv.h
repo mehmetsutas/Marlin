9530171a5e3b446d2b114769ba7e5a45e3e2bb78
Mehmet Sutas <msutas@rigid3d.com>
2020-11-07 18:46:23 +0300

    COnfiguration Düzeltmesi

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2328,10 +2328,10 @@
 
   #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
     // TMC2209: 0...255. TMC2130: -64...63
-    #define X_STALL_SENSITIVITY  90
+    #define X_STALL_SENSITIVITY  75
     #define X2_STALL_SENSITIVITY X_STALL_SENSITIVITY
-    #define Y_STALL_SENSITIVITY  90
-    #define Z_STALL_SENSITIVITY  135
+    #define Y_STALL_SENSITIVITY  75
+    #define Z_STALL_SENSITIVITY  130
     //#define SPI_ENDSTOPS              // TMC2130 only
     #define IMPROVE_HOMING_RELIABILITY
   #endif
@@ -2786,7 +2786,7 @@
  *  - M206 and M428 are disabled.
  *  - G92 will revert to its behavior from Marlin 1.0.
  */
-#define NO_WORKSPACE_OFFSETS
+//#define NO_WORKSPACE_OFFSETS
 
 /**
  * Set the number of proportional font spaces required to fill up a typical character space.
