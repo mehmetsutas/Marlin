d6dcb54b36951763eacb3ccf12c6bf032591be2f
Mehmet Sutaş <46458775+mehmetsutas@users.noreply.github.com>
2022-03-04 11:27:51 +0300

    Disable Leds

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2672,7 +2672,7 @@
  *
  * LED Type. Enable only one of the following two options.
  */
-#define RGB_LED
+//#define RGB_LED
 //#define RGBW_LED
 
 #if EITHER(RGB_LED, RGBW_LED)
@@ -2683,7 +2683,7 @@
 #endif
 
 // Support for Adafruit NeoPixel LED driver
-#define NEOPIXEL_LED
+//#define NEOPIXEL_LED
 #if ENABLED(NEOPIXEL_LED)
   #define NEOPIXEL_TYPE   NEO_GRB // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
   //#define NEOPIXEL_PIN     4       // LED driving pin
