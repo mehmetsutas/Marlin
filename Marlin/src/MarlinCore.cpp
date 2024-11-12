ee84b9c5e6f810aa995e257327f7dce42a57b0e2
Mehmet Sutaş <mehmet.sutas@sutasmakina.com>
2024-11-11 23:45:53 -0500

    Düzeltme

--- Marlin/src/MarlinCore.cpp
+++ Marlin/src/MarlinCore.cpp
@@ -820,7 +820,7 @@ void idle(bool no_stepper_sleep/*=false*/) {
     static uint8_t outage_counter = 0;
     if (recovery.enabled && (READ(POWER_LOSS_PIN) == POWER_LOSS_STATE))
       {
-        outage_counter++;
+        if (outage_counter<10) outage_counter++;
         if (outage_counter >= 3)
         {
             if (IS_SD_PRINTING()) abortSDPrinting(); //recovery.outage();
@@ -828,13 +828,13 @@ void idle(bool no_stepper_sleep/*=false*/) {
             thermalManager.set_fan_speed(0,255);
             if (thermalManager.degHotend(0)<50)
             {
-              OUT_WRITE(POWER_LOSS_BATTERY_PIN, !POWER_LOSS_BATTERY_ACTIVE_STATE);
+              WRITE(POWER_LOSS_BATTERY_PIN, !POWER_LOSS_BATTERY_ACTIVE_STATE);
             }
-        } else {
+        }
+      } else {
             outage_counter = 0;
             WRITE(POWER_LOSS_BATTERY_PIN, POWER_LOSS_BATTERY_ACTIVE_STATE);
         }
-      }
   #endif
 
   // Run StallGuard endstop checks
