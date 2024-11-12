d1b1fa186e8a0c3851ad06c0aa1ff6e5fad81744
Mehmet Sutaş <mehmet.sutas@sutasmakina.com>
2024-11-11 22:56:02 -0500

    config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1571,7 +1571,7 @@
     #define BACKUP_POWER_SUPPLY       // Backup power / UPS to move the steppers on power loss
     //#define POWER_LOSS_ZRAISE       0 // (mm) Z axis raise on resume (on power loss with UPS)
     #define POWER_LOSS_PIN      2 //P1_26 // Pin to detect power loss. Set to -1 to disable default pin on boards without module.
-    #define POWER_LOSS_STATE     LOW // State of pin indicating power loss
+    #define POWER_LOSS_STATE     1 // State of pin indicating power loss
     #define POWER_LOSS_BATTERY_PIN 15 //P1_28
     #define POWER_LOSS_BATTERY_ACTIVE_STATE 1
     //#define POWER_LOSS_PULLUP         // Set pullup / pulldown as appropriate for your sensor
